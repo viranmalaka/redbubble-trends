c5d38cae9e72d021caec94a32253fc6eddbfb725
viranmalaka <malaka.14@cse.mrt.ac.lk>
2023-06-29 18:31:57 +0530

    update title

--- redbubble-trends/app/layout.js
+++ redbubble-trends/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Redbubble Trends</title>
         <script
           async
           type="text/javascript"
