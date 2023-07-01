28cdc2b25659b53475849e724fc5f32e47a6b5ce
viranmalaka <malaka.14@cse.mrt.ac.lk>
2023-07-01 11:16:51 +0530

    open redbubble.

--- redbubble-trends/components/screape-modal.js
+++ redbubble-trends/components/screape-modal.js
@@ -29,7 +29,9 @@ function ScrapeModal() {
           // Copy the text inside the text field
           navigator.clipboard.writeText(content);
           setVisible(false);
-          window.open("https://redbubble.com", "_blank");
+          setTimeout(() => {
+            window.open("https://redbubble.com", "_blank");
+          }, 1000);
         }}
       >
         <div style={{ height: 600, overflow: "auto" }}>
